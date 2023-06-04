300b8cc293178dcbe7f85f395b25eea0ce9e2354
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-06-04 18:06:05 +0200

    added buttons to remove tasks and to mark tasks as done

--- JS/script.js
+++ JS/script.js
@@ -10,16 +10,48 @@
         },
     ]
 
+    const addNewTask = (newTask) => {
+        tasks.push({ content: newTask });
+        render();
+    }
+
+    const removeTask = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                tasks.splice(taskIndex, 1);
+                render();
+            })
+        })
+    }
+
+    const setTaskDone = () => {
+        const taskDoneButtons = document.querySelectorAll(".js-done");
+        taskDoneButtons.forEach((taskDoneButton, taskIndex) => {
+            taskDoneButton.addEventListener("click", () => {
+                tasks[taskIndex].done = !tasks[taskIndex].done;
+                render();
+            })
+        })
+    }
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}
-            >${task.content}</li>`;
+            >
+            <button class="js-done">Done?</button>
+            ${task.content}
+            <button class="js-remove">Remove</button>
+            </li>`;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        removeTask();
+        setTaskDone();
     }
 
     const onFormSubmit = (event) => {
@@ -31,11 +63,6 @@
         }
 
         addNewTask(newTask);
-    }
-
-    const addNewTask = (newTask) => {
-        tasks.push({ content: newTask });
-        render();
     }
 
     const init = () => {
