9087818497b9415bdfa6a85a0ef418a3b490c62d
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-06-04 12:59:47 +0200

    added render function to render array with objects content

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,28 @@
 {
-    const init = () => {
+    const tasks = [
+        {
+            content: "Prepare dinner",
+            done: false,
+        },
+        {
+            content: "Finish project",
+            done: false,
+        },
+    ]
 
-    };
+    const render = () => {
+        let htmlString = "";
+
+        for(const task of tasks) {
+            htmlString += `<li>${task.content}</li>`;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    }
+
+    const init = () => {
+        render();
+    }
 
     init();
 }
