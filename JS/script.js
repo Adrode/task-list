418c9c0c724012597512cd98d769e523fca9a769
Adrian Wozniak <adrianwozniak20@gmail.com>
2023-07-23 11:52:31 +0200

    changed setTaskDone function code to apply immutability convention

--- JS/script.js
+++ JS/script.js
@@ -19,10 +19,13 @@
     }
 
     const setTaskDone = (taskIndex) => {
-        /*tasks[taskIndex].done = !tasks[taskIndex].done;*/
         tasks = [
-            ...tasks,
-            tasks.map(tasks => !tasks[taskIndex].done),
+            ...tasks.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
+            },
+            ...tasks.slice(taskIndex + 1),
         ];
         render();
     }
